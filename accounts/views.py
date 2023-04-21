f70d61e3c13713eff1a22f7bfaeb566fe11ffceb
Reza <rdam9161@gmail.com>
2023-04-21 21:41:52 +0430

    custom user

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+
+
+# Create your views here.
--- test_shop/settings.py
+++ test_shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # My Apps
     'home',
+    'accounts',
 
 ]
 
@@ -134,3 +135,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User Model
+AUTH_USER_MODEL = 'accounts.CustomUser'
