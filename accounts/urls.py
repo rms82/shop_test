4e5bf14220cfec483566e5dd632a0e729d4b203a
Reza <rdam9161@gmail.com>
2023-04-28 17:02:01 +0430

    add log out

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('checkcode/<str:token>/', views.CheckCodeView.as_view(), name='checkcode'),
     # path('login/', views.login_view, name='login')
 ]
